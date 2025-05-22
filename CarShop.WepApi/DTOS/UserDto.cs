b46132a8eb158c81daf0e794f58bab20e13a4a8a
JavidGasim <gasimovcavid777@gmail.com>
2025-05-22 12:25:08 +0400

    current user dto changed

--- CarShop.WepApi/DTOS/UserDto.cs
+++ CarShop.WepApi/DTOS/UserDto.cs
@@ -6,7 +6,7 @@
         public string? UserName { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? ImagePath { get; set; }
+        public string? ProfilePicture { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? City { get; set; }
